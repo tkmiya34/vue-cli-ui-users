9b0096e6dd16acd0e675b46536bbc8643031d170
reishimoda <rei_s@libinc.co.jp>
2019-07-25 19:06:14 +0900

    User一覧を取得

--- src/views/User.vue
+++ src/views/User.vue
@@ -3,3 +3,22 @@
     <h1>This is an user page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'user',
+  data: function() {
+    return {
+      users: []
+    };
+  },
+  created: function() {
+    var self = this;
+    axios.get('https://randomuser.me/api/?results=10')
+      .then(function (response) {
+        console.log(response);
+        self.users.push(response.data.results);
+      });
+  }
+}
+</script>
